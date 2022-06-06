7befe7f0b61215de1459c652ac7ed5456c032c4b
ademuyiwa stephanie <stephanieademuyiwa1@gmail.com>
2022-06-06 17:32:34 +0100

    the second

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -13,7 +13,15 @@ BOOKS_PER_SHELF = 8
 #     If you do not update the endpoints, the lab will not work - of no fault of your API code!
 #   - Make sure for each route that you're thinking through when to abort and with which kind of error
 #   - If you change any of the response body keys, make sure you update the frontend to correspond.
+def paginate_books(request, selection):
+    page = request.args.get("page", 1, type=int)
+    start = (page - 1) * BOOKS_PER_SHELF
+    end = start + BOOKS_PER_SHELF
 
+    books = [book.format() for book in selection]
+    current_books = books[start:end]
+
+    return current_books
 
 def create_app(test_config=None):
     # create and configure the app
@@ -38,6 +46,22 @@ def create_app(test_config=None):
     #         update the frontend to handle additional books in the styling and pagination
     #         Response body keys: 'success', 'books' and 'total_books'
     # TEST: When completed, the webpage will display books including title, author, and rating shown as stars
+    @app.route('/books', methods=['GET'])
+    #@cross_origin
+    def get_books():
+        # Implement pagination
+        selection = Book.query.order_by(Book.id).all()
+        current_books = paginate_books(request, selection)
+
+        if len(current_books) == 0:
+            abort(404)
+
+        return jsonify(
+            {
+                "success": True,
+                "books": current_books,
+                "total_books": len(Book.query.all()),
+            })
 
     # @TODO: Write a route that will update a single book's rating.
     #         It should only be able to update the rating, not the entire representation
@@ -55,5 +79,33 @@ def create_app(test_config=None):
     #        Response body keys: 'success', 'created'(id of created book), 'books' and 'total_books'
     # TEST: When completed, you will be able to a new book using the form. Try doing so from the last page of books.
     #       Your new book should show up immediately after you submit it at the end of the page.
+    
+    @app.route("/books", methods=["POST"])
+    def create_book():
+        body = request.get_json()
+
+        new_title = body.get("title", None)
+        new_author = body.get("author", None)
+        new_rating = body.get("rating", None)
+
+        try:
+            book = Book(title=new_title, author=new_author, rating=new_rating)
+            book.insert()
+
+            selection = Book.query.order_by(Book.id).all()
+            current_books = paginate_books(request, selection)
+
+            return jsonify(
+                {
+                    "success": True,
+                    "created": book.id,
+                    "books": current_books,
+                    "total_books": len(Book.query.all()),
+                }
+            )
+
+        except:
+            abort(422)
+
 
     return app
